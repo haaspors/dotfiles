a8b6c70be2cceb1de7abaeb38eca8e199123b5f2
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2015-08-03 13:13:34 +0200

    env: Add natural scrolling for touchpads.

--- linux/env.sh
+++ linux/env.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 gsettings set org.gnome.desktop.default-applications.terminal exec x-terminal-emulator
+
+# Gnome touchpad natural scrolling
+gsettings set org.gnome.settings-daemon.peripherals.touchpad scroll-method 'two-finger-scrolling'
+gsettings set org.gnome.settings-daemon.peripherals.touchpad natural-scroll true
