98a07af840443e020cb473ac16274266b58ece38
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2016-10-04 15:32:53 +0200

    linux: Remap caps lock to ctrl.

--- linux/env.sh
+++ linux/env.sh
@@ -2,6 +2,9 @@
 
 gsettings set org.gnome.desktop.default-applications.terminal exec x-terminal-emulator
 
+# Caps lock -> Ctrl
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+
 # Gnome touchpad natural scrolling
 gsettings set org.gnome.settings-daemon.peripherals.touchpad scroll-method 'two-finger-scrolling'
 gsettings set org.gnome.settings-daemon.peripherals.touchpad natural-scroll true
