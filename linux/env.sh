cf95d74d22d70d1aa34f04066de1098d428ef5fc
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2019-02-12 10:33:17 +0100

    linux: Add gnome keyboard setting tweaks

--- linux/env.sh
+++ linux/env.sh
@@ -4,3 +4,7 @@ gsettings set org.gnome.desktop.default-applications.terminal exec x-terminal-em
 
 # Caps lock -> Ctrl
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+
+# Keyboard repeat setup
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30
+gsettings set org.gnome.desktop.peripherals.keyboard delay 250
